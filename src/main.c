10fa994db8c074253f7108a92c33b586144bb45d
Jakub Zahálka <36343898+chazp246@users.noreply.github.com>
2022-01-20 21:14:37 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -2,8 +2,10 @@
 #include "milis.h"
 
 /*#include "delay.h"*/
+//#include <cstdint>
 #include <stdio.h>
-/*#include "uart1.h"*/
+#include "spse_stm8.h"
+#include "../lib/uart.c"
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
@@ -12,7 +14,7 @@
 #define LED_PIN  GPIO_PIN_5
 #define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
 #define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
+#define LED_TOGG GPIO_WriteReverse(LED_PORT, LED_PIN)
 
 #define BTN_PORT GPIOE
 #define BTN_PIN  GPIO_PIN_4
@@ -23,28 +25,58 @@ void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-
     init_milis();
-    /*init_uart1();*/
+    init_uart();
+
+
+    // na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4, DISABLE);
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5, DISABLE);
+
+    // při inicializaci volíme frekvenci AD převodníku mezi 1-4MHz při 3.3V
+    // mezi 1-6MHz při 5V napájení
+    // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+    ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
+
+    // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+    ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
+
+    // nasatvíme multiplexer na některý ze vstupních kanálů
+    ADC2_Select_Channel(ADC2_CHANNEL_4);
+    // rozběhneme AD převodník
+    ADC2_Cmd(ENABLE);  
+    // počkáme než se AD převodník rozběhne (~7us)
+    ADC2_Startup_Wait();  
+
 }
+
 
 
 int main(void)
 {
     uint32_t time = 0;
+    uint16_t rawADC = 0;
+    uint32_t napeti = 0;
+    uint32_t teplota = 0;
 
     setup();
 
     while (1) {
 
-        if (milis() - time > 333 && BTN_PUSH) {
-            LED_REVERSE; 
+        if (milis() - time > 1000 ) {
+            LED_TOGG; 
             time = milis();
-            /*printf("%ld\n", time);*/
+            rawADC = ADC_get(ADC2_CHANNEL_4);
+            napeti = rawADC * (uint32_t)3300 / 1024;
+            teplota = (napeti - 0.4) / 0.0195;
+
+
+            printf("rawADC je %d\r\n", rawADC);
+            printf("napeti je %ld\r\n", napeti);
+            printf("teplota je %ld\r\n", teplota);
         }
 
-        /*LED_REVERSE; */
+        /*LED_FLIP; */
         /*_delay_ms(333);*/
         /*printf("Funguje to!!!\n");*/
     }
