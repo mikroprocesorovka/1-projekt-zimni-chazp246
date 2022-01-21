f52592de75c12745642513a669ba34f8d6a441aa
chazp246 <gameboys820@gmail.com>
2022-01-21 22:02:42 +0100

    vypada to nadejne

--- src/main.c
+++ src/main.c
@@ -1,113 +1,121 @@
 #include "stm8s.h"
 #include "milis.h"
-
-/*#include "delay.h"*/
-//#include <cstdint>
 #include <stdio.h>
 #include "spse_stm8.h"
 #include "uart1.h"
-//#include "../lib/uart.c"
-
+//#include "delay.h"
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
 
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
-#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_TOGG GPIO_WriteReverse(LED_PORT, LED_PIN)
 
+//vypnutí alarmu?
 #define BTN_PORT GPIOE
 #define BTN_PIN  GPIO_PIN_4
 #define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
 
 
-#define LIPO_PIN1 GPIO_PIN0
+//definování pinů na kterých bude připojena baterie
+//hlavní GND pin balančního konektoru
+#define LIPO_PIN_GND GPIO_PIN_0
+#define LIPO_PORT_GND GPIOB
+//1. článek
+#define LIPO_PIN1 GPIO_PIN_1
 #define LIPO_PORT1 GPIOB
-
-#define LIPO_PIN2 GPIO_PIN1
+//2. článek
+#define LIPO_PIN2 GPIO_PIN_2
 #define LIPO_PORT2 GPIOB
-
-#define LIPO_PIN3 GPIO_PIN2
+//3. článek
+#define LIPO_PIN3 GPIO_PIN_3
 #define LIPO_PORT3 GPIOB
-
-#define LIPO_PIN4 GPIO_PIN3
+//4. článek
+#define LIPO_PIN4 GPIO_PIN_4
 #define LIPO_PORT4 GPIOB
-
-#define LIPO_PIN5 GPIO_PIN4
+//5. článek
+#define LIPO_PIN5 GPIO_PIN_5
 #define LIPO_PORT5 GPIOB
-
-#define LIPO_PIN6 GPIO_PIN5
+//6. článek
+#define LIPO_PIN6 GPIO_PIN_6
 #define LIPO_PORT6 GPIOB
 
-#define LIPO_PIN7 GPIO_PIN6
-#define LIPO_PORT7 GPIOB
 
-
-
-
-
-
-void setup(void)
-{
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    init_milis();
-    init_uart1();
-
-
-    // na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
+void init_ADC(void){
+    //na kanálech 0 - 5 (PB1 - PB 6) vypne výstupní buffer
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL0, DISABLE);
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL1, DISABLE);
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL2, DISABLE);
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL3, DISABLE);
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4, DISABLE);
     ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5, DISABLE);
-
-    // při inicializaci volíme frekvenci AD převodníku mezi 1-4MHz při 3.3V
-    // mezi 1-6MHz při 5V napájení
-    // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+    //volba frekvence AD převodníku + CLOCK signálu
     ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
-
-    // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+    //zarovnání
     ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
-
-    // nasatvíme multiplexer na některý ze vstupních kanálů
+    //nastavení multiplexeru ?? co vlastně dělá? nebo v čem je jinej? SPL to má fakt divně vyřešený, se to volá jen jednou i když bych měřil jiný kanál...
     ADC2_Select_Channel(ADC2_CHANNEL_4);
-    // rozběhneme AD převodník
+    //samotné zapnutí AD převodníku
     ADC2_Cmd(ENABLE);  
-    // počkáme než se AD převodník rozběhne (~7us)
+    //menší startup delay pro převodník
     ADC2_Startup_Wait();  
-
 }
+
+
+
+
+void setup(void){
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); //Nastavení prescaleru
+    init_milis(); //inicializuje knihovnu milis
+    init_uart1(); //inicializuje VESTAVĚNÝ uart1
+    init_ADC(); //inicializace ADC
+}
+
 
 void all (void){
-    
-    ADC2_Select_Channel(ADC2_CHANNEL_4);
-    ADC2_Select_Channel(ADC2_CHANNEL_4);
-    ADC2_Select_Channel(ADC2_CHANNEL_4);
-    ADC2_Select_Channel(ADC2_CHANNEL_4);
-    ADC2_Select_Channel(ADC2_CHANNEL_4);
-    ADC2_Select_Channel(ADC2_CHANNEL_4);
-    ADC2_Select_Channel(ADC2_CHANNEL_4);
+    GPIO_Init(LIPO_PORT1, LIPO_PIN1, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(LIPO_PORT1, LIPO_PIN1);
 
+    clanek1 = ADC_get(ADC2_CHANNEL_1);
+    GPIO_Init(LIPO_PORT2, LIPO_PIN2, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(LIPO_PORT2, LIPO_PIN2);
+
+    GPIO_Init(LIPO_PORT3, LIPO_PIN3, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(LIPO_PORT3, LIPO_PIN3);
+
+    GPIO_Init(LIPO_PORT4, LIPO_PIN4, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(LIPO_PORT4, LIPO_PIN4);
+
+    GPIO_Init(LIPO_PORT5, LIPO_PIN5, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(LIPO_PORT5, LIPO_PIN5);
+
+    GPIO_Init(LIPO_PORT6, LIPO_PIN6, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(LIPO_PORT6, LIPO_PIN6);
+
+    GPIO_Init(LIPO_PORT_GND, LIPO_PIN_GND, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(LIPO_PORT_GND, LIPO_PIN_GND);
+    
+    //GPIO_Init(LIPO_PIN7, LIPO_PORT7, GPIO_MODE_IN_FL_NO_IT);
 }
 
 
 
-int main(void)
-{
+int main(void){
     uint32_t time = 0;
     uint16_t rawADC = 0;
     uint32_t napeti = 0;
     uint32_t teplota = 0;
+    uint32_t clanek1 = 0;
+    uint32_t clanek2 = 0;
+    uint32_t clanek3 = 0;
+    uint32_t clanek4 = 0;
+    uint32_t clanek5 = 0;
+    uint32_t clanek6 = 0;
+    uint32_t test = 0;
+
 
     setup();
+    ////////////////dal jsi sem 0 tak to pak nehledej
+    while (0) { 
 
-    while (1) {
-
-        if (milis() - time > 1000 ) {
-            LED_TOGG; 
+        if (milis() - time > 1000 ) { 
             time = milis();
             rawADC = ADC_get(ADC2_CHANNEL_4);
             napeti = rawADC * (uint32_t)3300 / 1024;
@@ -122,6 +130,17 @@ int main(void)
         /*LED_FLIP; */
         /*_delay_ms(333);*/
         /*printf("Funguje to!!!\n");*/
+    }
+    
+    
+    while(1){
+        if (napeti == 0){
+            GPIO_Init(LIPO_PORT_GND, LIPO_PIN_GND, GPIO_MODE_OUT_PP_LOW_SLOW);
+            GPIO_WriteLow(LIPO_PORT_GND, LIPO_PIN_GND);
+            test = ADC_get(ADC2_CHANNEL_0);
+            GPIO_WriteLow(LIPO_PORT_GND, LIPO_PIN_GND);
+            napeti = 1;
+        }
     }
 }
 
